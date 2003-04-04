ad9c8bfd566d77a87a0c26b363f06041d4dfcc79
Henri Yandell <bayard@apache.org>
2003-04-04 15:52:47 +0000

    Converts Strings to URLs.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/beanutils/trunk@128705 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/beanutils/converters/URLConverterTestCase.java
@@ -0,0 +1,156 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//beanutils/src/test/org/apache/commons/beanutils/converters/URLConverterTestCase.java,v 1.1 2003/04/04 15:52:47 bayard Exp $
+ * $Revision: 1.1 $
+ * $Date: 2003/04/04 15:52:47 $
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.commons.beanutils.converters;
+
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+import org.apache.commons.beanutils.Converter;
+
+import java.net.URL;
+
+
+/**
+ * Test Case for the URLConverter class.
+ *
+ * @author Henri Yandell
+ * @version $Revision: 1.1 $ $Date: 2003/04/04 15:52:47 $
+ */
+
+public class URLConverterTestCase extends TestCase {
+
+    private Converter converter = null;
+
+    // ------------------------------------------------------------------------
+
+    public URLConverterTestCase(String name) {
+        super(name);
+    }
+    
+    // ------------------------------------------------------------------------
+
+    public void setUp() throws Exception {
+        converter = makeConverter();
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(URLConverterTestCase.class);        
+    }
+
+    public void tearDown() throws Exception {
+        converter = null;
+    }
+
+    // ------------------------------------------------------------------------
+    
+    protected Converter makeConverter() {
+        return new URLConverter();
+    }
+    
+    protected Class getExpectedType() {
+        return URL.class;
+    }
+
+    // ------------------------------------------------------------------------
+
+    public void testSimpleConversion() throws Exception {
+        String[] message= { 
+            "from String",
+            "from String",
+            "from String",
+            "from String",
+            "from String",
+            "from String",
+            "from String",
+            "from String",
+        };
+        
+        Object[] input = { 
+            "http://www.apache.org",
+            "http://www.apache.org/",
+            "ftp://cvs.apache.org",
+            "file://project.xml",
+            "http://208.185.179.12",
+            "http://www.apache.org:9999/test/thing",
+            "http://user:admin@www.apache.org:50/one/two.three",
+            "http://notreal.apache.org",
+        };
+        
+        URL[] expected = { 
+            new URL("http://www.apache.org"),
+            new URL("http://www.apache.org/"),
+            new URL("ftp://cvs.apache.org"),
+            new URL("file://project.xml"),
+            new URL("http://208.185.179.12"),
+            new URL("http://www.apache.org:9999/test/thing"),
+            new URL("http://user:admin@www.apache.org:50/one/two.three"),
+            new URL("http://notreal.apache.org")
+        };
+        
+        for(int i=0;i<expected.length;i++) {
+            assertEquals(message[i] + " to URL",expected[i],converter.convert(URL.class,input[i]));
+            assertEquals(message[i] + " to null type",expected[i],converter.convert(null,input[i]));
+        }
+    }
+    
+}
+
