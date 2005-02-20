07cb355dfd5e7d0acd042ec563e7f4a84b8823c9
Niall Pemberton <niallp@apache.org>
2005-02-20 17:59:00 +0000

    Update JavaDocs
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/beanutils/trunk@154538 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/beanutils/IndexedTestBean.java
+++ src/test/org/apache/commons/beanutils/IndexedTestBean.java
@@ -36,53 +36,92 @@ public class IndexedTestBean {
 
     // ----------------------------------------------------------- Constructors
 
+    /**
+     * Default Constructor.
+     */
     public IndexedTestBean() {
     }
 
+    /**
+     * Getter for the String[] property.
+     */
     public String[] getStringArray() {
         return stringArray;
     }
 
+    /**
+     * Setter for the String[] property.
+     */
     public void setStringArray(String[] stringArray) {
         this.stringArray = stringArray;
     }
 
+    /**
+     * Indexed Getter for the String[] property.
+     */
     public String getStringArray(int index) {
         return (String)stringArray[index];
     }
 
+    /**
+     * Indexed Setter for the String[] property.
+     */
     public void setStringArray(int index, String value) {
         stringArray[index] = value;
     }
 
+    /**
+     * Getter for the java.util.List property.
+     */
     public List getStringList() {
         return stringList;
     }
 
+    /**
+     * Setter for the java.util.List property.
+     */
     public void setStringList(List stringList) {
         this.stringList = stringList;
     }
 
+    /**
+     * Indexed Getter for the java.util.List property.
+     */
     public String getStringList(int index) {
         return (String)stringList.get(index);
     }
 
+    /**
+     * Indexed Setter for the java.util.List property.
+     */
     public void setStringList(int index, String value) {
         stringList.add(index, value);
     }
 
+    /**
+     * Getter for the java.util.ArrayList property.
+     */
     public ArrayList getArrayList() {
         return arrayList;
     }
 
+    /**
+     * Setter for the java.util.ArrayList property.
+     */
     public void setArrayList(ArrayList arrayList) {
         this.arrayList = arrayList;
     }
 
+    /**
+     * Indexed Getter for the java.util.ArrayList property.
+     */
     public Object getArrayList(int index) {
         return arrayList.get(index);
     }
 
+    /**
+     * Indexed Setter for the java.util.ArrayList property.
+     */
     public void setArrayList(int index, Object value) {
         arrayList.add(index, value);
     }
