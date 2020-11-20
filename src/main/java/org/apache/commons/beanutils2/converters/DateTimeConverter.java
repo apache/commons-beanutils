f8d58d8239a569d99be16a5e9d76f55fd79f796c
Gary Gregory <garydgregory@gmail.com>
2020-11-20 16:52:09 -0500

    Use String.join().

--- src/main/java/org/apache/commons/beanutils2/converters/DateTimeConverter.java
+++ src/main/java/org/apache/commons/beanutils2/converters/DateTimeConverter.java
@@ -204,14 +204,7 @@ public abstract class DateTimeConverter extends AbstractConverter {
     public void setPatterns(final String[] patterns) {
         this.patterns = patterns;
         if (patterns != null && patterns.length > 1) {
-            final StringBuilder buffer = new StringBuilder();
-            for (int i = 0; i < patterns.length; i++) {
-                if (i > 0) {
-                    buffer.append(", ");
-                }
-                buffer.append(patterns[i]);
-            }
-            displayPatterns = buffer.toString();
+            displayPatterns = String.join(", ", patterns);
         }
         setUseLocaleFormat(true);
     }
