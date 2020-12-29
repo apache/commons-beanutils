2317f389169551f76a71c66328e7f109b309926f
Arturo Bernal <arturobernalg@gmail.com>
2020-12-29 22:19:09 +0100

    Remove Unused import

--- src/test/java/org/apache/commons/beanutils2/DynaBeanUtilsTestCase.java
+++ src/test/java/org/apache/commons/beanutils2/DynaBeanUtilsTestCase.java
@@ -20,7 +20,6 @@ package org.apache.commons.beanutils2;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
