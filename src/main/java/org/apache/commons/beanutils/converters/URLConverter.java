031774ebc5ff5a24057e3dc5f496f7245be0e48c
Gary Gregory <garydgregory@gmail.com>
2024-07-26 11:34:24 -0400

    Remove redundant call to super()

--- src/main/java/org/apache/commons/beanutils/converters/URLConverter.java
+++ src/main/java/org/apache/commons/beanutils/converters/URLConverter.java
@@ -34,7 +34,6 @@ public final class URLConverter extends AbstractConverter {
      * a <code>ConversionException</code> if an error occurs.
      */
     public URLConverter() {
-        super();
     }
 
     /**
