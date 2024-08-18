fd02cff627fe8fc803c8052ba402fec45660a24a
Gary Gregory <garydgregory@gmail.com>
2024-08-18 08:43:06 -0400

    Use else if

--- src/main/java/org/apache/commons/beanutils/locale/LocaleBeanUtilsBean.java
+++ src/main/java/org/apache/commons/beanutils/locale/LocaleBeanUtilsBean.java
@@ -322,18 +322,16 @@ public class LocaleBeanUtilsBean extends BeanUtilsBean {
             else {
                 newValue = value;
             }
+        // Value into scalar
+        } else if (value instanceof String) {
+            newValue = ConvertUtils.convert((String) value, type);
         }
-        else {                             // Value into scalar
-            if (value instanceof String) {
-                newValue = ConvertUtils.convert((String) value, type);
-            }
-            else if (value instanceof String[]) {
-                newValue = ConvertUtils.convert(((String[]) value)[0],
-                        type);
-            }
-            else {
-                newValue = value;
-            }
+        else if (value instanceof String[]) {
+            newValue = ConvertUtils.convert(((String[]) value)[0],
+                    type);
+        }
+        else {
+            newValue = value;
         }
         return newValue;
     }
@@ -381,18 +379,15 @@ public class LocaleBeanUtilsBean extends BeanUtilsBean {
             else {
                 newValue = value;
             }
+        } else if (value instanceof String) {
+            newValue = getLocaleConvertUtils().convert((String) value, type, pattern);
         }
-        else {                             // Value into scalar
-            if (value instanceof String) {
-                newValue = getLocaleConvertUtils().convert((String) value, type, pattern);
-            }
-            else if (value instanceof String[]) {
-                newValue = getLocaleConvertUtils().convert(((String[]) value)[0],
-                        type, pattern);
-            }
-            else {
-                newValue = value;
-            }
+        else if (value instanceof String[]) {
+            newValue = getLocaleConvertUtils().convert(((String[]) value)[0],
+                    type, pattern);
+        }
+        else {
+            newValue = value;
         }
         return newValue;
     }
