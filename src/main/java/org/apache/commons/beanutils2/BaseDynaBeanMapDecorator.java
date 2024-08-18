89c99d4743cf3b4659b3395af5bb2f63be6c725d
Gary Gregory <garydgregory@gmail.com>
2024-08-18 07:35:07 -0400

    Use final

--- src/main/java/org/apache/commons/beanutils2/BaseDynaBeanMapDecorator.java
+++ src/main/java/org/apache/commons/beanutils2/BaseDynaBeanMapDecorator.java
@@ -63,7 +63,7 @@ public abstract class BaseDynaBeanMapDecorator<K> implements Map<K, Object> {
         }
 
         @Override
-        public boolean equals(Object obj) {
+        public boolean equals(final Object obj) {
             if (this == obj) {
                 return true;
             }
