dd00c5a442bfac5227dc8d4a6b4e2f37cfe9540a
Gary D. Gregory <garydgregory@gmail.com>
2025-05-19 08:14:21 -0400

    Use an existing import instead of a FQCN

--- src/main/java/org/apache/commons/beanutils2/DefaultBeanIntrospector.java
+++ src/main/java/org/apache/commons/beanutils2/DefaultBeanIntrospector.java
@@ -48,7 +48,7 @@ public class DefaultBeanIntrospector implements BeanIntrospector {
     public static final BeanIntrospector INSTANCE = new DefaultBeanIntrospector();
 
     /** Constant for arguments types of a method that expects a list argument. */
-    private static final Class<?>[] LIST_CLASS_PARAMETER = new Class[] { java.util.List.class };
+    private static final Class<?>[] LIST_CLASS_PARAMETER = new Class[] { List.class };
 
     /** For logging. Each subclass gets its own log instance. */
     private final Log log = LogFactory.getLog(getClass());
